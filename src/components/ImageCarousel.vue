0626fc0eb2d0cb1b038004a0232c2b99b22eca9f
Brian Speight <brspeight@expedia.com>
2018-06-10 18:32:32 -0700

    New component to handle image carousels of screenshots

---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="screens">
+    {{ videos.length }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageCarousel',
+  props: ['screens']
+}
+</script>
+
+<style>
+  .screens {
+    border: 1px solid red;
+  }
+  .screen {
+    display: flex;
+  }
+  .screen-image {
+    width: 75%;
+    border: 1px solid green;
+    padding: 15px;
+  }
+  .screen-descr {
+    border: 1px solid orange;
+    padding: 15px;
+    overflow: hidden;
+    width: 25%
+  }
+</style>
