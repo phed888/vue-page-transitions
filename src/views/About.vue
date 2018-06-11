e677f303836210c0cb2b74450f78887e6ff10b1f
Brian Speight <brspeight@expedia.com>
2018-06-10 18:34:23 -0700

    Adding image carousel to the about page to consume about.json

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about page">
     <h1>This is an about page</h1>
+    <ImageCarousel v-bind:screens="screens"></ImageCarousel>
   </div>
 </template>
+
+<script>
+import ImageCarousel from '../components/ImageCarousel'
+import aboutJSON from '../data/project-data/about.json'
+
+export default {
+  name: 'About',
+  components: 'ImageCarousel',
+  data() {
+    return {
+      screens: aboutJSON
+    }
+  }
+}
+</script>
+
